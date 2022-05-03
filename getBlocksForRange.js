849c1f3337fab66f0d18e96610dcaa2963b602b2
jpuri <jyotipuri@gmail.com>
2022-05-03 21:07:11 +0530

    improvements

--- getBlocksForRange.js
+++ getBlocksForRange.js
@@ -46,8 +46,36 @@ function sendRequest(provider, method, params, noOfTrials, resolve, reject) {
   })
 }
 
-function query(provider, method, params, noOfTrials = 0) {
+function sendAsync(provider, request) {
   return new Promise((resolve, reject) => {
-    sendRequest(provider, method, params, noOfTrials, resolve, reject)
-  })
+    provider.sendAsync(request, (error, response) => {
+      if (error) {
+        reject(error);
+      } else if (response.error) {
+        reject(response.error);
+      } else if (response.result) {
+        resolve(response.result);
+      } else {
+        reject(new Error("Result was empty"));
+      }
+    });
+  });
+}
+
+async function query(provider, method, params) {
+  for (let i = 0; i < 3; i++) {
+    try {
+      return await sendAsync(provider, {
+        id: 1,
+        jsonrpc: "2.0",
+        method,
+        params,
+      });
+    } catch (error) {
+      console.error(
+        `provider.sendAsync failed: ${error.stack ?? error.message ?? error}`
+      );
+    }
+  }
+  throw new Error(`Block not found for params: ${params}`);
 }
