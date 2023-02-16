9f45aa7086cfeafab0c007d47cec766b5efd6c7f
Jyoti Puri <jyotipuri@gmail.com>
2023-02-16 17:44:01 +0530

    Fix: not throw error if block for a block number is not found (#89)
    * Fix: not throw error if block for a block number is not found
    
    * Fix: not throw error if block for a hash is not found

--- test/getBlocksForRange.js
+++ test/getBlocksForRange.js
@@ -28,7 +28,7 @@ test("return block number if there is no error", async (t) => {
   t.end();
 });
 
-test("looks for a block number 3 times until throwing", async (t) => {
+test("not throw error even if it is not found and filter out null", async (t) => {
   const sendAsync = sinon
     .stub()
     .withArgs({
@@ -45,10 +45,10 @@ test("looks for a block number 3 times until throwing", async (t) => {
     .callsArgWith(1, null, { result: null });
   const provider = { sendAsync };
   try {
-    await getBlocksForRange({ provider, fromBlock: "0x1", toBlock: "0x1" });
-    t.fail("Promise resolved when it was not supposed to");
+    const result = await getBlocksForRange({ provider, fromBlock: "0x1", toBlock: "0x1" });
+    t.deepEquals(result, []);
   } catch (error) {
-    t.equal(error.message, 'Block not found for params: ["0x1",false]');
+    t.fail("Should not throw error if result is obtained");
   }
   t.end();
 });
